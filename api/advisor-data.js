40643ac0267489e4eb674a9a3b9f22c2dd96dc96
mford4444 <mark@castorabbott.com>
2025-05-01 10:40:06 -0400

    Update advisor-data.js

--- api/advisor-data.js
+++ api/advisor-data.js
@@ -2,34 +2,14 @@
 
 import pdf from 'pdf-parse';
 import https from 'https';
-import httpsRequest from 'https';
 
 module.exports = async (req, res) => {
-  const firmCRD = '120562'; // Known working CRD with brochure link on public page
+  const brochureUrl = 'https://files.adviserinfo.sec.gov/IAPD/Content/Common/crd_iapd_Brochure.aspx?BRCHR_VRSN_ID=822410'; // Direct working PDF link
+  const firmCRD = '287966'; // Creative Planning LLC
 
   try {
-    const summaryUrl = `https://adviserinfo.sec.gov/firm/summary/${firmCRD}`;
-    const html = await new Promise((resolve, reject) => {
-      httpsRequest.get(summaryUrl, (response) => {
-        let data = '';
-        response.on('data', chunk => (data += chunk));
-        response.on('end', () => {
-          console.error('SEC summary HTML:', data.slice(0, 1000));
-          resolve(data);
-        });
-      }).on('error', reject);
-    });
-
-    const brochureMatch = html.match(/\/IAPD\/Content\/Common\/crd_iapd_Brochure\.aspx\?BRCHR_VRSN_ID=\d+/);
-    if (!brochureMatch || brochureMatch.length === 0) {
-      return res.status(404).json({ error: 'ADV brochure link not found on summary page' });
-    }
-
-    const brochurePath = brochureMatch[0];
-    const brochureUrl = `https://adviserinfo.sec.gov${brochurePath}`;
-
     const buffer = await new Promise((resolve, reject) => {
-      httpsRequest.get(brochureUrl, (response) => {
+      https.get(brochureUrl, (response) => {
         if (response.statusCode !== 200) {
           return reject(new Error(`Failed to download brochure. Status code: ${response.statusCode}`));
         }
